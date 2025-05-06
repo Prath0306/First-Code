570a75311c17bd78344e0a6b7ff8ebad95a56343
Prath0306 <sojwalchaudhari12@gmai.com>
2025-05-06 17:54:57 +0530

    update

--- src/Component/Counter.jsx
+++ src/Component/Counter.jsx
@@ -19,7 +19,9 @@ function Counter() {
 
     return (
         <>
+        <h1>Welcome</h1>
             <h2>Counter :{count}</h2>
+
             <button type="button" className="btn btn-primary m-2" onClick={increment}>Increment</button>
             <button type="button" className="btn btn-secondary" onClick={decrement}>Decrement</button>
         </>
